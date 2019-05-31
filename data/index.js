80189e16606270e2c2885bff5258558beb4eb17c
RaneWallin <rane@ranewallin.com>
2019-05-30 20:11:36 -0700

    adding helper methods

---
+++ data/index.js
@@ -0,0 +1,16 @@
+const knex = require('knex');
+
+const config = require('../knexfile.js').development;
+const db = knex(config);
+
+const getDishes = () => db('dishes');
+const getRecipes = () => db('recipes');
+const addDish = dish => db('dishes').insert(dish);
+const getDish = id => db('dishes').where({ id: Number(id) });
+const addRecipe = recipe => db('recipes').insert(recipe);
+
+
+module.exports = {
+    getDishes,
+    getRecipes
+}
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const recipes = require('./routes/recipes');
 
 const server = express();
 server.use(express.json());
+
+server.use('/api/recipes', recipes);
 
 server.get('/', (req, res) => {
     return res.send('Yo yo yo');
